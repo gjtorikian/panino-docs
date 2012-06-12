8bc8b3db414cfa5933a218f6c513b4ae4f7a8607
Garen Torikian <gjtorikian@gmail.com>
2012-06-12 15:04:52 -0700

    Able to disable test

--- build.js
+++ build.js
@@ -1,6 +1,6 @@
 var panino = require("./lib/cli");
 
-panino.main(["./src/prototype", "-e", "js", "-f", "html", "-p", "./parseOptions.json", "-a", "./additionalObjs.json", "-o", "./out/", "-t", "Prototype Docs", "--skin", "./skins/goose/", "-s", "-r"], function(err) {
+panino.main(["./tests/prototype", "-e", "js", "-f", "html", "-p", "./parseOptions.json", "-a", "./additionalObjs.json", "-o", "./out/", "-t", "Prototype Docs", "--skin", "./skins/goose/", "-s", "-r"], function(err) {
     if (err) {
         console.error(err);
         process.exit(-1);
