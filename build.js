d51e194a48e35dacec55694ecdb1bd5b37646f9d
Garen Torikian <gjtorikian@gmail.com>
2012-06-23 21:43:19 -0700

    Better creation of json file

--- build.js
+++ build.js
@@ -6,3 +6,11 @@ panino.main(["./tests/prototype", "-e", "js", "-f", "html", "-p", "./parseOption
         process.exit(-1);
     }
 });
+
+/*
+panino.main(["./tests/prototype", "-e", "js", "-f", "ast", "-p", "./parseOptions.json", "-a", "./additionalObjs.json", "-o", "./out/", "-t", "Prototype Docs", "--skin", "./skins/goose/", "-s", "-r"], function(err) {
+    if (err) {
+        console.error(err);
+        process.exit(-1);
+    }
+});*/
